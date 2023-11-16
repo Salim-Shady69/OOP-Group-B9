19e7c89e97ef3bad4d6f54c87b375a31ae14375f
herr-ek <43437993+herr-ek@users.noreply.github.com>
2023-11-16 08:29:23 +0100

    Parametrisk typning av workshop

--- src/lab2/AutomotiveWorkshop.java
+++ src/lab2/AutomotiveWorkshop.java
@@ -3,7 +3,14 @@ package lab2;
 import lab1.Vehicle;
 
 public class AutomotiveWorkshop<T extends Vehicle>{
-    private LoadCarrier loadCarrier;
+    private LoadCarrier<T> loadCarrier;
 
+    public AutomotiveWorkshop() {
+        loadCarrier = new LoadCarrier<>();
+    }
+
+    public void add(T vehicle){
+        loadCarrier.load(vehicle);
+    }
 }
 
